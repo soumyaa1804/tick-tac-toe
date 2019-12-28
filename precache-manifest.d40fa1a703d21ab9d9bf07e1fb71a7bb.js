e0aed2c93f2c037b595cbc505f02d76118da8ec8
soumyaa1804 <soum.sharma169@gmail.com>
2019-12-28 11:34:02 +0530

    Updates

---
+++ precache-manifest.d40fa1a703d21ab9d9bf07e1fb71a7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff27df86f77cd916b74301680c5ed09b",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d94bffb88d7d995d86d0",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a7b4ec457bc60535d76f",
+    "url": "/tic-tac-toe/static/js/2.ec1c8245.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.ec1c8245.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d94bffb88d7d995d86d0",
+    "url": "/tic-tac-toe/static/js/main.0e332166.chunk.js"
+  },
+  {
+    "revision": "04c1d8c2c1b5e42d5448",
+    "url": "/tic-tac-toe/static/js/runtime-main.c5b9a5ed.js"
+  }
+]);
